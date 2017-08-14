a1e1f880eb203dae262d77775f0bbc9d37267b61
Alex Karp <alex.karp@libertymutual.com>
2017-08-13 19:38:16 -0700

    Homework #2. Added Clean button & a supposedly funny image

--- src/main/java/com/example/demo/controllers/CalcController.java
+++ src/main/java/com/example/demo/controllers/CalcController.java
@@ -35,44 +35,52 @@ public class CalcController
 	{
 		// Used by "M+" button: "Recall the last result from memory"
 		double memResult = 0.0;
+		// At this point "the last result from memory" is not defined --> give model the default value
 		model.addAttribute("mem", memResult);
 		
 		
 		if (operation.equals("sub")) {
 				Subtr sub = new Subtr(first, second);
+				model.addAttribute("mem", sub.subtr()); 
 				model.addAttribute("val", sub.subtr()); 
 				model.addAttribute("oper", "Subtraction");} 
 		
 		else if (operation.equals("add")) { 
 				Adder add = new Adder(first, second);
 				memResult = add.sum();
+				model.addAttribute("mem",  memResult);
+				model.addAttribute("mem", add.sum()); 
 				model.addAttribute("val", add.sum()); 
 				model.addAttribute("oper", add.opr());}
 		
 		else if (operation.equals("div")) { 
 				Division div = new Division(first, second);
+				model.addAttribute("mem", div.divn()); 
 				model.addAttribute("val", div.divn());	
 				model.addAttribute("oper", "Division");}
 		
 		else if (operation.equals("mlt")) { 
 				Multip mlt = new Multip(first, second);
+				model.addAttribute("mem", mlt.mult()); 
 				model.addAttribute("val", mlt.mult()); 
 				model.addAttribute("oper", "Multiplication");}
 		
 		else if (operation.equals("mod")) { 
 				Modulo mod = new Modulo(first, second);
+				model.addAttribute("mem", mod.modl()); 
 				model.addAttribute("val", mod.modl()); 
 				model.addAttribute("oper", "Find Remainder");}
 		
 		else if (operation.equals("exp")) { 
 				Exponentiation exp = new Exponentiation(first, second);
+				model.addAttribute("mem", exp.expn()); 
 				model.addAttribute("val", exp.expn()); 
 				model.addAttribute("oper", "Raise X to the power of y");}
 		
 		else if (operation.equals("mem")) {
 			    model.addAttribute("val", memResult);
 			    model.addAttribute("mem", memResult);
-			    model.addAttribute("oper", "use result from the last calculation");}
+			    model.addAttribute("oper", "back to clean slate");}			
 
 		return "calc/calc-result-DN";
 	}		
